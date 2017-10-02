0c4d56e81186d6e9787ef36e1c3d527f8532fa0e
Anton Davydov <antondavydov.o@gmail.com>
2017-10-02 22:21:18 +0300

    Introduce array type for arguments and options

--- spec/integration/third_party_gems_spec.rb
+++ spec/integration/third_party_gems_spec.rb
@@ -1,12 +1,12 @@
 RSpec.describe "Third-party gems" do
   it "allows to add a subcommand" do
     output = `foo generate webpack`
-    expect(output).to eq("generate webpack\n")
+    expect(output).to eq("generate webpack. Apps: []\n")
   end
 
   it "allows to invoke a subcommand via an inherited subcomand aliases" do
     output = `foo g webpack`
-    expect(output).to eq("generate webpack\n")
+    expect(output).to eq("generate webpack. Apps: []\n")
   end
 
   it "allows to override basic commands" do
@@ -17,5 +17,10 @@ RSpec.describe "Third-party gems" do
   it "allows to override a subcommand" do
     output = `foo sub command`
     expect(output).to eq("override from webpack\n")
+  end
+
+  it "allows to call array option" do
+    output = `foo generate webpack --apps=test,api,admin`
+    expect(output).to eq("generate webpack. Apps: [\"test\", \"api\", \"admin\"]\n")
   end
 end
